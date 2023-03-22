a8612f3628ab15976108cdfdfca32e80a8da564d
Maximilian McClelland <mwmttl@gmail.com>
2023-03-22 14:37:33 -0600

    Contact Page

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+$(document).ready(function () {
     const services = document.querySelectorAll(".service");
     const windowHeight = window.innerHeight;
 
     // Function to check if an element is in the viewport
     function isInViewport(element) {
         const rect = element.getBoundingClientRect();
-        return rect.top >= -windowHeight / 3 && rect.bottom <= (windowHeight * 4) / 3;
+        return (
+            rect.top >= -windowHeight / 3 && rect.bottom <= (windowHeight * 4) / 3
+        );
     }
 
     // Function to highlight elements in the viewport
     function highlightVisibleServices() {
-        services.forEach(service => {
+        services.forEach((service) => {
             if (isInViewport(service)) {
                 service.classList.add("highlight");
             } else {
@@ -25,5 +27,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // Listen to scroll events and highlight elements accordingly
     window.addEventListener("scroll", () => {
         highlightVisibleServices();
+    });
+
+    // Add jQuery code to handle menu toggle
+    $(".menu-toggle").click(function () {
+        $(".menu").css("left", function (index, value) {
+            return value === "0px" ? "-350px" : "0px";
+        });
     });
 });
